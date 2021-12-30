e756d538f1ea12bdfd2ec6c8b66dd8c43a60a32a
watervapour <watervapour@users.noreply.github.com>
2021-12-30 21:58:04 +1100

    Hemisphere diffuse
    Complete to 8.6

--- src/vec3.h
+++ src/vec3.h
@@ -120,4 +120,12 @@ inline vec3 random_unit_vector() {
 	return unit_vector(random_in_unit_sphere());
 }
 
+inline vec3 random_in_hemisphere(const vec3& normal) {
+	vec3 in_unit_sphere = random_in_unit_sphere();
+	if(dot(in_unit_sphere, normal) > 0.0) { // In the same hemisphere as the normal
+		return in_unit_sphere;
+	} else {
+		return -in_unit_sphere;
+	}
+}
 #endif

8335e40b1937abe7d17c6affbeb49d44fed2437f
watervapour <watervapour@users.noreply.github.com>
2021-12-30 18:07:17 +1100

    Add antialiasing
    Complete section 7.2

--- src/colour.h
+++ src/colour.h
@@ -5,11 +5,21 @@
 
 #include <iostream>
 
-void write_colour(std::ostream &out, colour pixel_colour) {
+void write_colour(std::ostream &out, colour pixel_colour, int samples_per_pixel) {
+	auto r = pixel_colour.x();	
+	auto g = pixel_colour.y();	
+	auto b = pixel_colour.z();	
+
+	// Divied the colour by the number of samples
+	auto scale = 1.0 / samples_per_pixel;
+	r *= scale;
+	g *= scale;
+	b *= scale;
+
 	// Write the translated [0, 255] value of each colour component.
-	out << static_cast<int>(255.999 * pixel_colour.x()) << ' '
-		<< static_cast<int>(255.999 * pixel_colour.y()) << ' '
-		<< static_cast<int>(255.999 * pixel_colour.z()) << '\n';
+	out << static_cast<int>(255 * clamp(r, 0.0, 0.999)) << ' '
+		<< static_cast<int>(255 * clamp(g, 0.0, 0.999)) << ' '
+		<< static_cast<int>(255 * clamp(b, 0.0, 0.999)) << '\n';
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,10 @@
-/*
-#include "rtweekend.h"
-#include "colour.h"
-#include "ray.h"
-#include "vec3.h"
-*/
 #include "rtweekend.h"
 #include "colour.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
-
-/*
-double hit_sphere(const point3& centre, double radius, const ray& r) {
-	vec3 oc = r.origin() - centre;
-	auto a = r.direction().length_squared();
-	auto half_b = dot(oc, r.direction());
-	auto c = oc.length_squared() - radius*radius;
-	auto discriminant = half_b*half_b - a*c;
-
-	if(discriminant < 0) {
-		return -1.0;
-	} else {
-		return (-half_b - sqrt(discriminant) ) / a;
-	}
-}
-*/
 
 colour ray_colour(const ray& r, const hittable& world) {
 	hit_record rec;
@@ -43,6 +22,7 @@ int main(int argc, char** argv){
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	// World
 	hittable_list world;
@@ -50,14 +30,7 @@ int main(int argc, char** argv){
 	world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
 	// Camera
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+	camera cam;
 
 	// Render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -65,11 +38,14 @@ int main(int argc, char** argv){
 	for(int j = image_height-1; j >=0; --j){
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for(int i = 0; i < image_width; ++i){
-			auto u = double(i) / (image_width-1);
-			auto v = double(j) / (image_height-1);
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-			colour pixel_colour = ray_colour(r, world);
-			write_colour(std::cout, pixel_colour);
+			colour pixel_colour(0, 0, 0);
+			for(int s = 0; s < samples_per_pixel; ++s){
+				auto u = (i + random_double()) / (image_width-1);
+				auto v = (j + random_double()) / (image_height-1);
+				ray r = cam.get_ray(u, v);
+				pixel_colour += ray_colour(r, world);
+			}
+			write_colour(std::cout, pixel_colour, samples_per_pixel);
 		}
 	}
 

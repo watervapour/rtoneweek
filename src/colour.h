c683082cceb6149914651013b7adf6cf28ccf75b
watervapour <watervapour@users.noreply.github.com>
2021-12-30 20:26:48 +1100

    Reduced noise
    Complete to 8.4

--- src/colour.h
+++ src/colour.h
@@ -10,11 +10,11 @@ void write_colour(std::ostream &out, colour pixel_colour, int samples_per_pixel)
 	auto g = pixel_colour.y();	
 	auto b = pixel_colour.z();	
 
-	// Divied the colour by the number of samples
+	// Divied the colour by the number of samples and gamma-correct for gamma=2.0.
 	auto scale = 1.0 / samples_per_pixel;
-	r *= scale;
-	g *= scale;
-	b *= scale;
+	r = sqrt(scale * r);
+	g = sqrt(scale * g);
+	b = sqrt(scale * b);
 
 	// Write the translated [0, 255] value of each colour component.
 	out << static_cast<int>(255 * clamp(r, 0.0, 0.999)) << ' '

76b4585d94779abb26c4a55bc14b4983807cabe5
watervapour <watervapour@users.noreply.github.com>
2022-01-05 19:14:56 +1100

    Updated image 13_1
    Image now has good resolution.

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ int main(int argc, char** argv){
 	const auto aspect_ratio = 3.0 / 2.0;
 	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 2;
+	const int samples_per_pixel = 500;
 	const int max_depth = 50;
 
 	// World
